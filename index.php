29eae496f3b6c57e9d5a454aaa9260a89d1469c8
Farph4 <131706508+Farph4@users.noreply.github.com>
2023-06-15 17:11:13 -0500

    z-toolin-v1
    La versión es inestable , sin errores (en desarrollo)

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Farph4 - Toolin</title>
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/settings.css">
+</head>
+<body>
+    <div id="container">
+
+        <div id="menu" class="rounded">
+
+            <div id="container-box">
+                <div class="shadow-lg p-3 m-2 rounded" title="Herramientas de Hacking "><i class="bi bi-motherboard-fill m-1"></i>Hacking</div>
+                <div class="shadow-lg p-3 m-2 rounded" title="Transformadores"><i class="bi bi-ev-front-fill m-1"></i>Transformadores</div>
+                <div class="shadow-lg p-3 m-2 rounded" title="Chatbot"><i class="bi bi-chat-dots m-1"></i>Chat</div>
+            </div>
+
+        </div>
+
+        <div id="view">
+
+        </div>
+
+    </div>
+    
+    <script src="js/app.js"></script>
+</body>
+</html>
